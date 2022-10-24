4f2d04f82fc02432958d0a9a2921543d24635886
FrancisHenriquez <francishenriquez07@gmail.com>
2022-10-24 16:41:57 -0400

    useRef primer uso

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { FocusScreen } from './04-useRef/FocusScreen'
 // import { FormWithCustomHook } from './02-UseEffect/FormWithCustomHook'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+
 // import { SimpleForm } from './02-UseEffect/SimpleForm'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { CounterApp } from './01-useState/CounterApp'
@@ -10,6 +12,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <MultipleCustomHooks/>
-  // </React.StrictMode>
+    <FocusScreen />
+  //  </React.StrictMode>
 )
