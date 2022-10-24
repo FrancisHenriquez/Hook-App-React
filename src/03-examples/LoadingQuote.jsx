12d77b51c92ccf0eba5beec277deb65ba08ed14e
FrancisHenriquez <francishenriquez07@gmail.com>
2022-10-24 16:04:45 -0400

    conectando ambos hooks y organizacion

---
+++ src/03-examples/LoadingQuote.jsx
@@ -0,0 +1,10 @@
+// import React from 'react'
+
+export const LoadingQuote = () => {
+  return (
+    <div className="lert aler-info text-center">
+    Loading...
+</div>
+  )
+}
+
--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,16 +1,28 @@
-
-import { useFetch } from "../Hooks/useFetch"
+import { useCounter, useFetch } from "../Hooks";
+import { LoadingQuote, Quote } from "./";
 
 export const MultipleCustomHooks = () => {
 
-const { data, isLoading, hasError } = useFetch (' https://www.breakingbadapi.com/api/quotes/1 ')
-    console.log({ data, isLoading, hasError })
+const{ counter, increment } = useCounter(1);    
+const { data, isLoading, hasError } = useFetch ( `https://www.breakingbadapi.com/api/quotes/${ counter }` )
+const {author, quote} = !!data && data[0]
+// console.log({ data, isLoading, hasError })
+
   return (
     <>  
     <h1>Breaking Bad Quotes </h1>
    <hr />
-
-    
+   {
+        isLoading
+            ? <LoadingQuote/>
+            : <Quote author={author} quote = { quote }/>
+   }
+  
+<LoadingQuote/>
+   <Quote/>
+   <button className="btn btn-primary" onClick={ () => increment() }>
+    Next quote
+   </button>
     </>
    
   )
