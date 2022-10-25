5e55df03f3f076b21aceffc7ac2e21961f5cae7d
FrancisHenriquez <francishenriquez07@gmail.com>
2022-10-25 10:22:06 -0400

    useMemo

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { Layout } from './05-useLayoutEffect/Layout'
-import { Memories } from './06-memos/Memories'
+// import { Memories } from './06-memos/Memories'
+import { MemoriesHook } from './06-memos/MemoriesHook'
 // import { FormWithCustomHook } from './02-UseEffect/FormWithCustomHook'
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 // import { SimpleForm } from './02-UseEffect/SimpleForm'
@@ -13,6 +14,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <Memories />
+    <MemoriesHook />
   //  </React.StrictMode>
 )
