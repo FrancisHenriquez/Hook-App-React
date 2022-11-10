64292957f09785e9142b6732caaa795b43319032
FrancisHenriquez <francishenriquez07@gmail.com>
2022-11-10 10:44:04 -0400

    m

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 // import { CallbackHook } from './06-memos/CallbackHook'
 import { Padre } from './07-tarea-memo/Padre'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { Layout } from './05-useLayoutEffect/Layout'
 // import { Memories } from './06-memos/Memories'
 // import { MemoriesHook } from './06-memos/MemoriesHook'
-// import { FormWithCustomHook } from './02-UseEffect/FormWithCustomHook'
+import { FormWithCustomHook } from './02-UseEffect/FormWithCustomHook'
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 // import { SimpleForm } from './02-UseEffect/SimpleForm'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
@@ -16,6 +17,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <Padre/>
+    <FormWithCustomHook/>
   //  </React.StrictMode>
 )
